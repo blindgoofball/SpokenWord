0cb9998c498bf8207f82dff5bf40c501ad88d197
Blindgoofball <blindgoofball@gmail.com>
2025-11-18 11:06:07 -0500

    Added sentence manager class.

---
+++ src/sentencemanager.py
@@ -0,0 +1,36 @@
+from random import choices
+import difflib
+import speech
+
+class SentenceManager:
+    def __init__(self):
+        self.sentences=self.pickSentences()
+        self.currentSentence=0
+        self.totalWords=0
+        self.mistakes=0
+    def loadSentences(self):
+        try:
+            with open('sentences.txt', 'r') as f:
+                sentences=f.readlines()
+        except FileNotFoundError:
+            return []
+        sentences=[s.strip() for s in sentences if s]
+        return sentences
+    def pickSentences(self, amount=30):
+        sentences=self.loadSentences()
+        return choices(sentences, k=30)
+    def finishSentence(self, value):
+        words=len(value.split())
+        self.totalWords+=words
+        self.checkMistakes(self.sentences[self.currentSentence], value)
+        self.currentSentence+=1
+        if self.currentSentence >= len(self.sentences):
+            return None
+        return self.sentences[self.currentSentence]
+    def checkMistakes(self, original, typed):
+        expected=original.split()
+        provided=typed.split()
+        matcher=difflib.SequenceMatcher(None, expected, provided)
+        for tag, i1, i2, j1, j2 in matcher.get_opcodes():
+            if tag in ['replace', 'delete', 'insert']:
+                self.mistakes+=1
