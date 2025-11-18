4e86b5a94bbc23d76177081da18df37c9cccad00
Blindgoofball <blindgoofball@gmail.com>
2025-11-18 12:47:15 -0500

    Added the ability to press the tab key to get the sentence spoken again.

--- src/app.py
+++ src/app.py
@@ -12,6 +12,7 @@ class SpokenWord(wx.Frame):
         self.sizer.Add(self.textField, 1, wx.ALL|wx.EXPAND)
         self.panel.SetSizer(self.sizer)
         self.textField.Bind(wx.EVT_TEXT, self.onText)
+        self.textField.Bind(wx.EVT_CHAR_HOOK, self.onKey)
         self.Show()
         speech.speak(self.sentenceManager.sentences[self.sentenceManager.currentSentence])
     def onText(self, evt):
@@ -23,6 +24,11 @@ class SpokenWord(wx.Frame):
                 speech.speak(nextSentence)
             else:
                 self.finish()
+    def onKey(self, evt):
+        if evt.GetKeyCode() == wx.WXK_TAB:
+            speech.speak(self.sentenceManager.sentences[self.sentenceManager.currentSentence])
+        else:
+            evt.Skip()
     def finish(self):
         accuracy=100-(self.sentenceManager.mistakes/self.sentenceManager.totalWords*100)
         stats=f"Complete! You had an accuracy of {accuracy:.1f}%."
